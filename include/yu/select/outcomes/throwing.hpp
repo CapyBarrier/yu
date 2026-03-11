bfbb43e22cb358c2af64be52bcff8e507d7d330d
CapyBarrier <kapusaishin65535@gmail.com>
2026-03-12 02:16:13 +0900

    [Yu.select] Removed the helpers for generating an object of OutcomePolicy

--- include/yu/select/outcomes/throwing.hpp
+++ include/yu/select/outcomes/throwing.hpp
@@ -1,11 +1,30 @@
 #ifndef YU_INCLUDE_YU_SELECT_OUTCOMES_THROWING_HPP_
 #define YU_INCLUDE_YU_SELECT_OUTCOMES_THROWING_HPP_
 
-#include "detail/throwing_t.hpp"
+#include <yu/select/policy_tags/outcome_policy_tag.hpp>
+#include <yu/select/select_error.hpp>
+#include <utility>
 
 namespace yu::select::outcomes {
 
-inline detail::throwing_t throwing{};
+struct throwing {
+        using outcome_policy_tag = policy_tags::outcome_policy_tag;
+
+        template <typename Result, typename T>
+        Result success(T&& result) const {
+            return std::forward<T>(result);
+        }
+
+        template <typename Result>
+        requires std::is_void_v<Result>
+        Result success() const {}
+
+        template <typename Result>
+        [[noreturn]]
+        Result failure() const {
+            throw select_error{"No clause was selected"};
+        }
+};
 
 } // namespace yu::select::outcomes
 
