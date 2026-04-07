bc0f3f47d99e167c39f7adff68c7816942da4ab3
CapyBarrier <kapusaishin65535@gmail.com>
2026-04-07 22:33:50 +0900

    [Yu.Tuples]index_sequence_forを実装

--- tests/tuples/index_sequence_for.cpp
+++ tests/tuples/index_sequence_for.cpp
@@ -1,6 +1,24 @@
-#include <yu/tuples/indes_sequence_for.hpp>
+#include "yu/tuples/get.hpp"
+#include "yu/tuples/size.hpp"
+#include <yu/tuples/index_sequence_for.hpp>
+#include <cassert>
+#include <string>
+#include <tuple>
+#include <utility>
+
+
+template <typename T, std::size_t... Idx>
+auto f(T&& tuple, std::index_sequence<Idx...>) {
+    return std::make_tuple(yu::tuples::get<yu::tuples::size_v<T> - Idx - 1>(tuple)...);
+}
+
+template <typename T>
+requires yu::tuples::tuple<T>
+auto g(T&& tup) {
+    return f(std::forward<T>(tup), yu::tuples::index_sequence_for<T>{});
+}
 
 int main() {
-    
+    assert(g(std::make_tuple(1, 'a', std::string{"Hello"})) == std::make_tuple(std::string{"Hello"}, 'a', 1));
     return 0;
 }
